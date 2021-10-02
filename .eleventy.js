adaff7bc3a6aa7a06d25a302f484229c9d020e80
Steve Della Valentina <s.dellavalentina@gmail.com>
2021-10-02 17:37:04 -0600

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/static");
+  eleventyConfig.addPassthroughCopy("./src/*.png");
 
   return {
     dir: {
