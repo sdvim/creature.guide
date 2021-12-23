1b56f9deb286f6ca0763be285f140c29936bd5de
Steve Della Valentina <s.dellavalentina@gmail.com>
2021-12-22 18:00:25 -0700

    Replace miniature.earth with THREE.js

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,7 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js'; 
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/OrbitControls.js'; 
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/GLTFLoader.js'; 
+
 import planetImages from "./planetImages.js";
 import * as Util from "./functions.js";
 
@@ -54,111 +58,55 @@ import * as Util from "./functions.js";
     }
   };
 
-  const setupEarth = () => {
-    const planetData = {
-      ercon: { location: { lat: 0, lng: -18 }, scale: 20, offset: 46 },
-      marius: { location: { lat: 20, lng: -90 }, scale: 20, offset: 47 },
-      gomba: { location: { lat: -30, lng: 60 }, scale: 22, offset: 45 },
-      uuary: { location: { lat: 30, lng: 130 }, scale: 22, offset: 48 },
-      inciu: { location: { lat: 10, lng: -160 }, scale: 25, offset: 42 },
-    };
-    const creaturePoses = ["laying", "sitting", "standing", "standingAlt"];
-    const maxCloudsPerPose = 3;
-    const starPoints = [];
-    const clouds = [];
-    const miniEarth = new Earth("earth", {
-      location: { lat: 10, lng: -80 },
-      light: "none",
-      mapSeaColor: "#1e4497",
-      mapLandColor: "#819647",
-      mapBorderWidth: 0,
-      mapStyles: "#AQ { fill: #f0e4d8; }",
-      autoRotate: true,
-      autoRotateDelay: 0,
-      autoRotateStart: 0,
-    });
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.querySelector('#earth').appendChild(renderer.domElement); 
 
-    // Add spaceObjects
-    for (const [name, data] of Object.entries(planetData)) {
-      spaceObjects[name] = miniEarth.addPoints({
-        points: [data],
-        image: planetImages[name],
-        visible: isDarkModeOn,
-      });
-    }
+  const light = new THREE.AmbientLight( 0xFFFFFF );
+  const geometry = new THREE.SphereGeometry(1, 50, 50);
+  const material = new THREE.MeshPhongMaterial({
+    map: new THREE.TextureLoader().load("../3d/earth@4x.png"),
+    shininess: 0
+  });
+  const sphere = new THREE.Mesh( geometry, material );
+  const controls = new OrbitControls( camera, renderer.domElement );
 
-    // Add clouds and creature models
-    creaturePoses.forEach(async (pose) => {
-      const cloudName = pose.substr(0, 8);
-      await fetch(`../3d/cloud-${cloudName}.obj`)
-        .then(response => response.blob())
-        .then(blob => blob.text())
-        .then(mesh => {
-          Earth.addMesh(mesh);
-          for (let i = 0; i < maxCloudsPerPose; i++) {
-            const targetScale = Math.max(0.08, Math.random() * 0.15);
-            let cloud = miniEarth.addMarker({
-              mesh: "Cloud" + cloudName.slice(0,1).toUpperCase() + cloudName.slice(1),
-              location: { lat: Util.randomInt(-70, 70), lng: Util.randomInt(-170, 170) },
-              rotationY: Util.randomInt(-60, 60),
-              color: '#fff',
-              scale: 0,
-              offset: 1,
-              opacity: 0.95,
-            });
-            cloud.animate("offset", 1.1, { duration: 0, relativeDuration: 6000 + (i * 1000), loop: true, oscillate: true });
-            setTimeout(async () => loadObjFile(
-              "../3d/",
-              `creature-${pose}.obj`,
-              `material.mtl`,
-              mesh => {
-                cloud.object3d.add(mesh);
-                cloud.animate("scale", targetScale, { duration: 300 });
-              }
-            ), 100 + (i * 400));
-            clouds.push(cloud);
-          }
-        });
-    });
+  const starTexture = new THREE.TextureLoader().load("/3d/stars.png");
+  starTexture.wrapS = THREE.RepeatWrapping;
+  starTexture.wrapT = THREE.RepeatWrapping;
+  starTexture.repeat.set( 6, 6 );
 
-    // Add stars
-    for (let i = 0; i < 1000; i++) {
-      starPoints.push({
-        location: { lat: Util.randomInt(-60, 60), lng: Util.randomInt(-180, 180) },
-        offset: Util.randomInt(52, 120),
-        scale: 1,
-        opacity: Util.randomInt(20, 80) / 100,
-        color: "#bab479",
-      });
-    }
-    spaceObjects.stars = miniEarth.addPoints({
-      points: starPoints,
-      scale: 0.25 + window.innerHeight / 1000,
-      visible: isDarkModeOn,
-    });
+  var starGeometry = new THREE.SphereGeometry(1000, 50, 50);
+  var starMaterial = new THREE.MeshPhongMaterial({
+    map: starTexture,
+    side: THREE.DoubleSide,
+    shininess: 0
+  });
+  var starField = new THREE.Mesh(starGeometry, starMaterial);
+  scene.add(starField);
 
+  scene.add( light )
+  scene.add( sphere );
 
-    let ticking = false;
-    const updateEarth = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          clouds.forEach((cloud, index) => {
-            let {lat, lng} = cloud.location;
-            lat += 0.03 + (cloud.rotationY / 500);
-            lng += 0.1 - 0.05 * cloud.scale;
-            cloud.location = { lat, lng };
-            cloud.lookAt = { lat, lng };
-          });
-          ticking = false;
-        });
-        ticking = true;
-      }
-    };
+  camera.position.z = 5;
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 0.05;
+  controls.minPolarAngle = Math.PI * 0.35;
+  controls.maxPolarAngle = Math.PI * 0.65;
+  controls.enablePan = false;
+  controls.enableZoom = false;
+  controls.update();
 
-    miniEarth.addEventListener("update", updateEarth);
+  function animate() {
+    requestAnimationFrame(animate);
+    sphere.rotation.y += 0.001;
+    controls.update();
+    renderer.render(scene, camera);
   }
+  animate();
 
-  window.addEventListener("earthjsload", setupEarth);
   window.addEventListener("load", onPageLoad);
   window.addEventListener("scroll", onPageScroll);
 })();
