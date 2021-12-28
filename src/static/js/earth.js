a248213d13fe44df0687ad77480a4ce2c6ac6cee
Steve Della Valentina <s.dellavalentina@gmail.com>
2021-12-28 15:04:41 -0700

    Refactor and clean up code

--- src/static/js/earth.js
+++ src/static/js/earth.js
@@ -1,9 +1,9 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js'; 
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/OrbitControls.js'; 
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/GLTFLoader.js';
 import { SkeletonUtils } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/utils/SkeletonUtils.js';
 
-const planetData = [
+const planets = [
   {
     name: 'ercon',
     x: -40,
@@ -36,221 +36,223 @@ const planetData = [
   },
 ];
 
-const walkers = [
+const creatures = [
   {
     // USA
     x: 0.85,
     z: 0.2,
+    type: 'walk',
   },
   {
     // LatAm
     x: 1.65,
     z: -0.5,
+    type: 'walk',
   },
   {
     // Africa
     x: 3.75,
     z: -1.75,
+    type: 'walk',
   },
   {
     // Asia
     x: -0.8,
+    type: 'walk',
   },
-];
-
-const swimmers = [
   {
     // South America
     x: 14.5,
     z: 0.2,
+    type: 'swim',
   },
   {
     // Hawaii
     x: 1,
     z: 1,
+    type: 'swim',
   },
   {
     // Atlantic Ocean
     x: 0.75,
     z: -0.7,
+    type: 'swim',
   },
   {
     // Indian Ocean
     x: 4.25,
     z: -0.25,
+    type: 'swim',
   },
   {
     // New Zealand
     x: -0.2,
     z: 2.35,
+    type: 'swim',
   },
 ];
 
+const creatureTypes = {
+  walk: {
+    animation: 'creature_walkonglobe',
+    scale: [1.0025, 1.05, 1.0025],
+    durationRange: [0.2, 0.6],
+  },
+  swim: {
+    animation: 'creature-swim',
+    scale: [1.0, 1.0, 1.0],
+    durationRange: [0.1, 0.3],
+  },
+};
+
 export default function Earth() {
-  let scene, camera, renderer, controls, sphereGeometry, sphereMaterial, sphere, starTexture, starGeometry, starMaterial, starField, group, clock, loader, delta;
-  let isRotating = false;
-  let isReadyToRotate = false;
+  const scene = new THREE.Scene();
+  const clock = new THREE.Clock();
+  const globe = new THREE.Group();
   const mixers = [];
-  const planets = [];
 
-  const earthEl = document.querySelector('#earth');
-  
-  scene = new THREE.Scene();
+  this.isRotating = false;
+  this.isReadyToRotate = false;
 
-  camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const sphere = (() => {
+    let geometry = new THREE.SphereGeometry(1, 50, 50);
+    let material = new THREE.MeshPhongMaterial({
+      map: new THREE.TextureLoader().load("../3d/earth@4x.png"),
+      shininess: 0,
+      transparent: true,
+    });
+    return new THREE.Mesh(geometry, material);
+  })();
+
+  const starParticles = (() => {
+    let geometry = new THREE.Geometry();
+    let material = new THREE.PointsMaterial({
+      color: 0xBAB479,
+      size: 2,
+      transparent: true,
+    });
+    let randCoordinate = () => {
+      return THREE.Math.randInt(50, 650) * ((Math.random() >= 0.5) ? 1 : -1);
+    };
+    for (var p = 0; p < 8831 / 2; p++) {
+      geometry.vertices.push(new THREE.Vector3(
+        randCoordinate(),
+        randCoordinate(),
+        randCoordinate(),
+      ));
+    }
+    return new THREE.Points(geometry, material);
+  })();
+
+  const planetSprites = (() => {
+    let sprites = [];
+    planets.forEach(({ name, x, y, z }) => {
+      let sprite = new THREE.Sprite(new THREE.SpriteMaterial({
+        map: new THREE.TextureLoader().load(`/img/${name}.png`)
+      }));
+      sprite.position.set(x, y, z);
+      sprites.push(sprite);
+    });
+    return sprites;
+  })();
+
+  //
+  // Event functions
+  //
+  const animate = () => {
+    requestAnimationFrame(animate);
+    let delta = clock.getDelta();
+    mixers.forEach((mixer) => {
+      mixer.update(delta);
+    });
+    globe.rotation.y += (this.isReadyToRotate && this.isRotating) ? 0.001 : 0;
+    controls.update();
+    renderer.render(scene, camera);
+  };
+
+  const onModelLoaded = (gltf) => {
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.05;
+    controls.update();
+    enableRotation();
+    this.isReadyToRotate = true;
+
+    setTimeout(() => {
+      this.el.classList.add('loaded');
+    }, 500);
+
+    creatures.forEach(({x, z, type}) => {
+      let [sX, sY, sZ] = creatureTypes[type].scale;
+      let [minTime, maxTime] = creatureTypes[type].durationRange;
+      let animation = creatureTypes[type].animation;
+      let creature = SkeletonUtils.clone(gltf.scene);
+      let mixer = new THREE.AnimationMixer(creature);
+      let clip = THREE.AnimationClip.findByName(gltf.animations, animation);
+      let action = mixer.clipAction(clip);
+
+      creature.children[0].scale.set(sX, sY, sZ);
+      creature.children[0].rotation.x = x || 0;
+      creature.children[0].rotation.z = z || 0;
+      mixer.timeScale = THREE.Math.randFloat(minTime, maxTime);
+
+      action.play();
+      mixers.push(mixer);
+      globe.add(creature);
+    });
+  };
+
+  const onWindowResize = () => {
+    const { width, height } = this.el.getBoundingClientRect();
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+    renderer.render(scene, camera);
+  };
+
+  const disableRotation = () => { this.isRotating = false; };
+  const enableRotation = () => { this.isRotating = true; };
+
+  //
+  // Public functions
+  //
+  this.toggleNight = (isVisible) => {
+    [...planetSprites, starParticles].forEach((obj) => {
+      obj.visible = isVisible;
+    });
+  };
+
+  //
+  // Setup
+  //
+
+  this.el = document.querySelector('.earth');
+  let { width, height } = this.el.getBoundingClientRect();
+
+  const camera = new THREE.PerspectiveCamera(25, width / height, 0.1, 1000);
   camera.position.z = 5.5;
 
-  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(width, height);
   renderer.setPixelRatio(window.devicePixelRatio);
-  
-  controls = new OrbitControls(camera, renderer.domElement);
+  this.el.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
   controls.minPolarAngle = Math.PI * 0.35;
   controls.maxPolarAngle = Math.PI * 0.65;
   controls.enablePan = false;
   controls.enableZoom = false;
   controls.update();
 
-  earthEl.appendChild(renderer.domElement); 
-
-  sphereGeometry = new THREE.SphereGeometry(1, 50, 50);
-  sphereMaterial = new THREE.MeshPhongMaterial({
-    map: new THREE.TextureLoader().load("../3d/earth@4x.png"),
-    shininess: 0,
-    transparent: true,
-  });
-  sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-  var particleCount = 3600,
-  particles = new THREE.Geometry(),
-  pMaterial = new THREE.PointsMaterial({
-    color: 0xbab479,
-    size: 2,
-    transparent: true,
-  });
-
-  // now create the individual particles
-  for (var p = 0; p < particleCount; p++) {
-
-
-    const randCoordinate = () => {
-      return THREE.Math.randInt(50, 650) * ((Math.random() >= 0.5) ? 1 : -1);
-    };
-  var pX = randCoordinate(),
-    pY = randCoordinate(),
-    pZ = randCoordinate(),
-    particle = new THREE.Vector3(pX, pY, pZ);
-
-  particles.vertices.push(particle);
-  }
-
-  var particleSystem = new THREE.Points(particles, pMaterial);
-
-  scene.add(particleSystem);
-
-
-  group = new THREE.Group();
-  clock = new THREE.Clock();
-
-  loader = new GLTFLoader().setPath('../3d/');
-  loader.load(
-    'CreatureWeb_Creature_WalkAndSwim.glb',
-    (gltf) => {
-
-      controls.autoRotate = true;
-      controls.autoRotateSpeed = 0.05;
-      controls.update();
-      isReadyToRotate = true;
-      isRotating = true;
-      setTimeout(() => {
-        earthEl.classList.add('loaded');
-      }, 500);
-
-      walkers.forEach(({x, z}) => {
-        const walker = SkeletonUtils.clone(gltf.scene);
-        walker.children[0].scale.set(1.0025, 1.05, 1.0025);
-        walker.children[0].rotation.x = x || 0;
-        walker.children[0].rotation.z = z || 0;
-
-				const mixer = new THREE.AnimationMixer(walker);
-				const clip = THREE.AnimationClip.findByName(gltf.animations, 'creature_walkonglobe');
-        const action = mixer.clipAction(clip);
-        mixer.timeScale = THREE.Math.randFloat(0.2, 0.6);
-        action.play();
-        mixers.push(mixer);
-
-        group.add(walker);
-      });
-      
-      swimmers.forEach(({x, z}) => {
-        const walker = SkeletonUtils.clone(gltf.scene);
-        walker.children[0].scale.set(1.0, 1.0, 1.0);
-        walker.children[0].rotation.x = x || 0;
-        walker.children[0].rotation.z = z || 0;
-
-				const mixer = new THREE.AnimationMixer(walker);
-				const clip = THREE.AnimationClip.findByName(gltf.animations, 'creature-swim');
-        const action = mixer.clipAction(clip);
-        mixer.timeScale = THREE.Math.randFloat(0.1, 0.3);
-        action.play();
-        mixers.push(mixer);
-
-        group.add(walker);
-      });
-    },
-  );
-
-  planetData.forEach(({ name, x, y, z }) => {
-    const sprite = new THREE.Sprite(new THREE.SpriteMaterial({
-      map: new THREE.TextureLoader().load(`/img/${name}.png`)
-    }));
-    sprite.position.set(x, y, z);
-    planets.push(sprite);
-    scene.add(sprite);
-  });
-
-
-  group.add(sphere);
+  globe.add(sphere);
+  scene.add(starParticles);
+  scene.add(...planetSprites);
   scene.add(new THREE.AmbientLight(0xFFFFFF));
-  scene.add(group);
-
-
-  function animate() {
-    requestAnimationFrame(animate);
-    delta = clock.getDelta();
-    mixers.forEach((mixer) => {
-      mixer.update(delta);
-    });
-    group.rotation.y += (isReadyToRotate && isRotating) ? 0.001 : 0;
-    controls.update();
-    renderer.render(scene, camera);
-  }
-
-  function onWindowResize() {
-    const { width, height } = earthEl.getBoundingClientRect();
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(width, height);
-    renderer.render(scene, camera);
-  }
-
-  controls.addEventListener('start', () => {
-    isRotating = false;
-  });
-
-  controls.addEventListener('end', () => {
-    isRotating = true;
-  });
-
+  scene.add(globe);
   animate();
   onWindowResize();
 
   window.addEventListener('resize', onWindowResize);
-
-  this.toggleNight = (isVisible) => {
-    [...planets, particleSystem].forEach((obj) => {
-      obj.visible = isVisible;
-    });
-  };
+  controls.addEventListener('start', disableRotation);
+  controls.addEventListener('end', enableRotation);
+  new GLTFLoader().setPath('../3d/').load('CreatureWeb_Creature_WalkAndSwim.glb', onModelLoaded);
 }
