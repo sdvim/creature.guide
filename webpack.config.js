612d9be69058c3aabc7240cbde01edfb54743c9e
Steve Della Valentina <s.dellavalentina@gmail.com>
2021-12-28 09:08:14 -0700

    Revert "Add webpack"
    This reverts commit c135ae99d0aa5a23e1d7249aa7d98c0950bcaeab.

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-  entry: './src/static/js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'public/js'),
-    filename: 'main.js'
-  }
-};
